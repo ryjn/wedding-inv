909ff6083ec2c6f6c0732c0c574a091b1ab7712b
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-04-01 01:14:31 -0400

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { Home } from "./components/sections/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="wrapper">
+        <Home />
+      </div>
+    </>
+  );
 }
 
 export default App;
