6e34966c153cf33b2118abeffaeb7caba3cd23ec
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-04-08 22:07:42 -0400

    spacing

--- src/components/sections/Map.jsx
+++ src/components/sections/Map.jsx
@@ -12,7 +12,7 @@ export const Map = () => {
         <div className="w-full h-auto">
           <img src={MapImg} className="" />
         </div>
-        <div className="flex-1 space-y-5">
+        <div className="flex-1 space-y-10">
           <p className="text-center text-base md:text-2xl font-light">
             함께 바라본 순간들이
             <br />
@@ -30,7 +30,7 @@ export const Map = () => {
             당신의 따뜻한 발걸음을 기다립니다.
           </p>
         </div>
-        <div className="flex-1 pt-4">
+        <div className="flex-1 pt-15">
           <h3 className="pb-4 text-base md:text-2xl text-center">
             축하의 마음 전하시는 곳
           </h3>
@@ -41,18 +41,26 @@ export const Map = () => {
             신부측
           </h3>
           <div className="flex-grow border-t border-gray-300 mx-4"></div>
-          <p className="text-base md:text-2xl text-right font-light">
-            국민 정성윤 774202-01-182819
-          </p>
+          <div className="text-base md:text-2xl text-right font-light">
+            <p className="text-left">
+              국민 정성윤
+              <br />
+              774202-01-182819
+            </p>
+          </div>
         </div>
         <div className="flex w-[90%] md:w-[70%] justify-between items-center">
           <h3 className="text-base md:text-2xl text-left font-medium">
             신랑측
           </h3>
           <div className="flex-grow border-t border-gray-300 mx-4"></div>
-          <p className="text-base md:text-2xl text-right font-light">
-            219910-56-953407
-          </p>
+          <div className="text-base md:text-2xl text-right font-light">
+            <p className="text-left">
+              Bank info/Name
+              <br />
+              219910-56-953407
+            </p>
+          </div>
         </div>
       </section>
     </RevealOnScroll>
