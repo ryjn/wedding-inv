ef73b280238fce4d24793a2fd2818ef3a94c58df
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-04-08 22:00:51 -0400

    account info

--- src/components/sections/Map.jsx
+++ src/components/sections/Map.jsx
@@ -30,11 +30,29 @@ export const Map = () => {
             당신의 따뜻한 발걸음을 기다립니다.
           </p>
         </div>
-        <div className="flex-1">
-          <h3 className="py-4 text-base md:text-2xl text-center">
+        <div className="flex-1 pt-4">
+          <h3 className="pb-4 text-base md:text-2xl text-center">
             축하의 마음 전하시는 곳
           </h3>
           <img src={DownArrow} className="w-3 md:w-4 h-auto mx-auto" />
+        </div>
+        <div className="flex w-[90%] md:w-[70%] justify-between items-center">
+          <h3 className="text-base md:text-2xl text-left font-medium">
+            신부측
+          </h3>
+          <div className="flex-grow border-t border-gray-300 mx-4"></div>
+          <p className="text-base md:text-2xl text-right font-light">
+            국민 정성윤 774202-01-182819
+          </p>
+        </div>
+        <div className="flex w-[90%] md:w-[70%] justify-between items-center">
+          <h3 className="text-base md:text-2xl text-left font-medium">
+            신랑측
+          </h3>
+          <div className="flex-grow border-t border-gray-300 mx-4"></div>
+          <p className="text-base md:text-2xl text-right font-light">
+            219910-56-953407
+          </p>
         </div>
       </section>
     </RevealOnScroll>
