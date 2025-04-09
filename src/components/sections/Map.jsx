d60a22291ba89ef4de604e1c8e7ae10f48b7b3d0
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-04-09 01:24:57 -0400

    fixed map section screen height

--- src/components/sections/Map.jsx
+++ src/components/sections/Map.jsx
@@ -7,7 +7,7 @@ export const Map = () => {
     <RevealOnScroll>
       <section
         id="map"
-        className="h-[calc(100vh-65px)] flex flex-col items-center justify-between relative gap-5"
+        className="min-h-screen flex flex-col items-center justify-between relative gap-5"
       >
         <div className="w-full h-auto">
           <img src={MapImg} className="" />
