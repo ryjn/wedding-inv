fcc5b92fcdad331e9447324a389a9ed6c0e8ca13
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-04-08 23:07:40 -0400

    consisten font sizing and spacing

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -10,29 +10,29 @@ export const Home = () => {
         id="home"
         className="min-h-screen py-5 flex flex-col items-center justify-between relative"
       >
-        <h3 className="text-base md:text-2xl text-center font-light">
+        <h3 className="text-base md:text-xl text-center font-light">
           해가 길어지는 저녁,
           <br />
           저희의 시작을 함께해 주세요
         </h3>
         <img src={TopLine} className="w-auto h-36 md:h-auto" />
         <div className="w-[90%] md:w-[70%] flex flex-row justify-between items-center">
-          <div className="flex-1 flex-row text-left space-y-1">
-            <h3 className="text-xs md:text-base font-sansKR">신랑</h3>
+          <div className="flex-1 flex-row text-left space-y-1 md:space-y-3">
+            <h3 className="text-sm md:text-base font-sansKR">신랑</h3>
             <h1 className="text-2xl md:text-5xl font-nanum tracking-wider">
               김준
             </h1>
           </div>
-          <div className="flex-1 flex-row text-center space-y-1">
-            <h3 className="text-2xl md:text-3xl font-nanum text-[var(--color-accent)]">
+          <div className="flex-1 flex-row text-center space-y-1 md:space-y-3">
+            <h3 className="text-xl md:text-3xl font-nanum text-[var(--color-accent)]">
               05
             </h3>
             <hr className="border-t border-[var(--color-accent)] w-7 md:w-8 mx-auto" />
-            <h3 className="text-2xl md:text-3xl font-nanum text-[var(--color-accent)]">
+            <h3 className="text-xl md:text-3xl font-nanum text-[var(--color-accent)]">
               31
             </h3>
           </div>
-          <div className="flex-1 flex-row text-right space-y-1">
+          <div className="flex-1 flex-row text-right space-y-1 md:space-y-3">
             <h3 className="text-sm md:text-base font-sansKR">신부</h3>
             <h1 className="text-2xl md:text-5xl font-nanum tracking-tighter">
               정성윤
@@ -41,20 +41,20 @@ export const Home = () => {
         </div>
         <img src={BottomLine} className="w-auto h-36 md:h-auto" />
         <div className="text-center space-y-3">
-          <h2 className="text-xl md:text-3xl font-nanum font-medium">
+          <h2 className="text-lg md:text-2xl font-nanum font-medium">
             2025.05.31
           </h2>
           <div className="">
-            <h3 className="text-base md:text-2xl font-light">
+            <h3 className="text-base md:text-xl font-light">
               오후 5시, 오가헌
             </h3>
-            <h3 className="text-base md:text-2xl font-light">
+            <h3 className="text-base md:text-xl font-light">
               광주광역시 동구 금남로4가 42
             </h3>
           </div>
         </div>
         <div className="flex flex-col">
-          <h3 className="py-4 text-base md:text-2xl text-center">
+          <h3 className="py-4 text-base md:text-xl text-center">
             오가헌 찾아오시는 길
           </h3>
           <img src={DownArrow} className="w-3 md:w-4 h-auto mx-auto" />
--- src/components/sections/Map.jsx
+++ src/components/sections/Map.jsx
@@ -13,35 +13,33 @@ export const Map = () => {
           <img src={MapImg} className="" />
         </div>
         <div className="flex-1 space-y-10">
-          <p className="text-center text-base md:text-2xl font-light">
+          <p className="text-center text-base md:text-xl font-light">
             함께 바라본 순간들이
             <br />
             조용히 한 계절을 이루었습니다.
           </p>
-          <p className="text-center text-base md:text-2xl font-light">
+          <p className="text-center text-base md:text-xl font-light">
             노을이 물든 저녁,
             <br />
             새로운 계절에
             <br />첫 걸음을 내딛습니다.
           </p>
-          <p className="text-center text-base md:text-2xl font-light">
+          <p className="text-center text-base md:text-xl font-light">
             그 시작의 자리에
             <br />
             당신의 따뜻한 발걸음을 기다립니다.
           </p>
         </div>
         <div className="flex-1 pt-15">
-          <h3 className="pb-4 text-base md:text-2xl text-center">
+          <h3 className="pb-4 text-base md:text-xl text-center">
             축하의 마음 전하시는 곳
           </h3>
           <img src={DownArrow} className="py-4 w-3 md:w-4 h-auto mx-auto" />
         </div>
         <div className="flex w-[90%] md:w-[70%] justify-between items-center">
-          <h3 className="text-base md:text-2xl text-left font-medium">
-            신부측
-          </h3>
+          <h3 className="text-base md:text-xl text-left font-medium">신부측</h3>
           <div className="flex-grow border-t border-gray-300 mx-4"></div>
-          <div className="text-sm md:text-2xl text-right font-light">
+          <div className="text-base md:text-xl text-right font-light">
             <p className="text-left">
               국민 정성윤
               <br />
@@ -50,11 +48,9 @@ export const Map = () => {
           </div>
         </div>
         <div className="flex w-[90%] md:w-[70%] justify-between items-center pb-9">
-          <h3 className="text-base md:text-2xl text-left font-medium">
-            신랑측
-          </h3>
+          <h3 className="text-base md:text-xl text-left font-medium">신랑측</h3>
           <div className="flex-grow border-t border-gray-300 mx-4"></div>
-          <div className="text-sm md:text-2xl text-right font-light">
+          <div className="text-base md:text-xl text-right font-light">
             <p className="text-left">
               하나 박정희
               <br />
